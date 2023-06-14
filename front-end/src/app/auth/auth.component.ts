fe1d20bc360f5e5432907037661cb33ba310d63e
Skylow667 <nathan.locussol@gmail.com>
2023-06-14 15:38:11 +0200

    get access_token 42 api

--- front-end/src/app/auth/auth.component.ts
+++ front-end/src/app/auth/auth.component.ts
@@ -14,17 +14,22 @@ export class AuthComponent implements OnInit {
     const code: string | null= urlParams.get('code');
     console.log("CODE:", code);
     if (code) {
-
+      let accessToken: string;
       fetch("https://api.intra.42.fr/oauth/token",{
-      method: 'POST',
-      body: JSON.stringify({
+      method: "POST",
+      body: new URLSearchParams({
         grant_type: "authorization_code",
         client_id: "u-s4t2ud-d4f9852c6392f3a567c8fb78fac0ffaa6a248187093e5a84ba0a0b1e507c8f01",
-        secret_id: "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",
+        client_secret: "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",
         code: code,
         redirect_uri: "http://localhost:4200/auth"
+      }),
+    }).then(reponse => reponse.json())
+      .then(reponseBis => {
+        console.table(reponseBis)
+        accessToken = reponseBis.access_token
       })
-      })
+      .catch(error => console.log(error))
     }
   }
 }
