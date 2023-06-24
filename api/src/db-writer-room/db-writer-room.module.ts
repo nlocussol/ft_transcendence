49fa629c2c346549310a38bf552eda58080e1cd9
Armandase <damiens.armand@gmail.com>
2023-06-24 11:16:51 +0200

    fix lil issue

--- api/src/db-writer-room/db-writer-room.module.ts
+++ api/src/db-writer-room/db-writer-room.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { DbWriterRoomService } from './db-writer-room.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DbWriterRoomController } from './db-writer-room.controller';
-import { Room } from 'src/typeorm';
+import { Room, User } from 'src/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Room])],
+  imports: [TypeOrmModule.forFeature([Room]), TypeOrmModule.forFeature([User])],
   providers: [DbWriterRoomService],
   controllers: [DbWriterRoomController]
 })
