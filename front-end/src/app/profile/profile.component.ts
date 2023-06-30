12d5a10b6df8557b5be786593ce9e85880902bbe
Louis Truchelut <louis.truchelut@gmail.com>
2023-06-30 16:06:09 +0200

    Fixed f5 problem in profile

--- front-end/src/app/profile/profile.component.ts
+++ front-end/src/app/profile/profile.component.ts
@@ -5,12 +5,13 @@ import { environment } from 'src/environment';
 import { Socket, io } from 'socket.io-client';
 import { Friend, UserData } from '../chat-room/interfaces/interfaces';
 import { Notif, addFriend } from './interfaces/interfaces';
+import { HomeService } from '../home/service/home.service';
 
 @Component({
   selector: 'app-profile',
-  template: `{{message}}`,
+  template: `{{ message }}`,
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit, OnDestroy {
   doubleAuth!: boolean;
@@ -18,7 +19,7 @@ export class ProfileComponent implements OnInit, OnDestroy {
   newPseudo!: string;
   pseudoFriend!: string;
   profileData!: UserData;
-  pseudo: string = "! Go Login to get a profile card !";
+  pseudo!: string;
   login!: string;
   ppUrl!: string;
   status!: string;
@@ -26,19 +27,23 @@ export class ProfileComponent implements OnInit, OnDestroy {
   notifs!: Notif[];
 
   ngOnInit(): void {
-    this.socket = io(environment.SOCKET_ENDPOINT);
-    this.login = this.dataService.getLogin();
-    this.pseudo = this.dataService.getPseudo();
-    console.log('My pseudo:', this.pseudo);
-    this.getProfileData();
-    this.newNotif();
+    this.homeService.getUser().subscribe((res) => {
+      this.login = res.login;
+      this.getProfileData();
+      console.log(this.login, this.pseudo);
+      this.socket = io(environment.SOCKET_ENDPOINT);
+      this.newNotif();
+    });
   }
 
   ngOnDestroy(): void {
     this.socket.disconnect();
   }
 
-  constructor(private http: HttpClient, private dataService: DataService) {}
+  constructor(
+    private http: HttpClient,
+    private homeService: HomeService
+  ) {}
 
   onFileSelected(event: any): void {
     this.selectedFile = event.target.files[0];
@@ -49,107 +54,142 @@ export class ProfileComponent implements OnInit, OnDestroy {
     reader.readAsDataURL(this.selectedFile);
     const body = {
       login: this.login,
-      newPp: this.selectedFile
-    }
-    const headers = new HttpHeaders().set('Content-type', `application/json`)
-    this.http.post("http://localhost:3000/db-writer/change-user-pp/", body, { headers }).subscribe()
+      newPp: this.selectedFile,
+    };
+    const headers = new HttpHeaders().set('Content-type', `application/json`);
+    this.http
+      .post('http://localhost:3000/db-writer/change-user-pp/', body, {
+        headers,
+      })
+      .subscribe();
   }
 
   change2AF() {
     this.doubleAuth = !this.doubleAuth;
     const body = {
       login: this.login,
-      doubleAuth: this.doubleAuth
-    }
-    const headers = new HttpHeaders().set('Content-type', `application/json`)
-    this.http.post("http://localhost:3000/db-writer/change-2fa/", body, { headers }).subscribe()
+      doubleAuth: this.doubleAuth,
+    };
+    const headers = new HttpHeaders().set('Content-type', `application/json`);
+    this.http
+      .post('http://localhost:3000/db-writer/change-2fa/', body, { headers })
+      .subscribe();
   }
 
   async changeUsername() {
     const body = {
       currentPseudo: this.pseudo,
-      newPseudo: this.newPseudo
-    }
-    const headers = new HttpHeaders().set('Content-type', `application/json`)
-    const res = await this.http.post("http://localhost:3000/db-writer/change-user-pseudo/", body, { headers }).toPromise()
-    if (res)
-      this.pseudo = this.newPseudo;
+      newPseudo: this.newPseudo,
+    };
+    const headers = new HttpHeaders().set('Content-type', `application/json`);
+    const res = await this.http
+      .post('http://localhost:3000/db-writer/change-user-pseudo/', body, {
+        headers,
+      })
+      .toPromise();
+    if (res) this.pseudo = this.newPseudo;
   }
 
   acceptRequest(body: Notif) {
     const bodyToDelete = {
       login: this.login,
-      index: this.notifs.findIndex(notif => notif === body)
-    }
+      index: this.notifs.findIndex((notif) => notif === body),
+    };
     let bodyToSend: any;
-    const headers = new HttpHeaders().set('Content-type', `application/json`)
+    const headers = new HttpHeaders().set('Content-type', `application/json`);
     switch (body.type) {
       case 'REQUEST_FRIEND':
         bodyToSend = {
           login: this.login,
           friend: body.login,
-        }
-        this.http.post("http://localhost:3000/db-writer/add-friend/", bodyToSend as addFriend, { headers }).subscribe()
+        };
+        this.http
+          .post(
+            'http://localhost:3000/db-writer/add-friend/',
+            bodyToSend as addFriend,
+            { headers }
+          )
+          .subscribe();
         break;
 
       case 'REQUEST_MATCH':
         bodyToSend = {
           uuid: crypto.randomUUID(),
           payer1: this.login,
-          player2: bodyToDelete.login
-        }
+          player2: bodyToDelete.login,
+        };
         const notifBody = {
           friend: body.login,
           login: this.login,
           content: `${this.pseudo} has accepted your match request so go in the game page!`,
-          type: 'MATCH_ACCEPTED'
-        }
+          type: 'MATCH_ACCEPTED',
+        };
         this.socket.emit('send-notif', notifBody);
-        break ;
+        break;
 
       case 'ROOM_INVITE':
         bodyToSend = {
           name: body.name,
           login: this.login,
           pseudo: this.pseudo,
-        }
-        this.http.post('http://localhost:3000/db-writer-room/add-user-room', bodyToSend, { headers }).subscribe()
+        };
+        this.http
+          .post(
+            'http://localhost:3000/db-writer-room/add-user-room',
+            bodyToSend,
+            { headers }
+          )
+          .subscribe();
         break;
     }
-    this.notifs.splice(this.notifs.findIndex(request => body === request), 1);
-    this.http.post('http://localhost:3000/db-writer/delete-notif/', bodyToDelete, { headers }).subscribe()
+    this.notifs.splice(
+      this.notifs.findIndex((request) => body === request),
+      1
+    );
+    this.http
+      .post('http://localhost:3000/db-writer/delete-notif/', bodyToDelete, {
+        headers,
+      })
+      .subscribe();
   }
 
   refuseRequest(body: Notif) {
     const bodyToDelete = {
       login: this.login,
-      index: this.notifs.findIndex(notif => notif === body)
-    }
-    const headers = new HttpHeaders().set('Content-type', `application/json`)
-    this.notifs.splice(this.notifs.findIndex(request => body === request), 1);
-    this.http.post('http://localhost:3000/db-writer/delete-notif/', bodyToDelete, { headers }).subscribe()
+      index: this.notifs.findIndex((notif) => notif === body),
+    };
+    const headers = new HttpHeaders().set('Content-type', `application/json`);
+    this.notifs.splice(
+      this.notifs.findIndex((request) => body === request),
+      1
+    );
+    this.http
+      .post('http://localhost:3000/db-writer/delete-notif/', bodyToDelete, {
+        headers,
+      })
+      .subscribe();
   }
 
   async getProfileData() {
-    this.profileData = await this.http.get(`http://localhost:3000/db-writer/data/${this.login}`).toPromise() as UserData
+    this.profileData = (await this.http
+      .get(`http://localhost:3000/db-writer/data/${this.login}`)
+      .toPromise()) as UserData;
     this.ppUrl = this.profileData.pp;
-    this.notifs = this.profileData.notif
+    this.notifs = this.profileData.notif;
     this.status = this.profileData.status;
-    this.doubleAuth = this.profileData.doubleAuth
-    if (this.pseudo !== this.profileData.pseudo)
-      this.pseudo = this.profileData.pseudo
+    this.doubleAuth = this.profileData.doubleAuth;
+    this.pseudo = this.profileData.pseudo;
   }
 
   newNotif() {
     this.socket.on('receive-notif', (data: Notif) => {
       if (data.friend === this.login) {
-        if (!this.notifs)
-          this.notifs = [];
+        if (!this.notifs) this.notifs = [];
         if (data.login)
           // this.notifs.push({friend: data.login, content: data.content, type: data.type});
           this.notifs.push(data);
       }
-    })
+    });
   }
 
   handleFriendSubmit() {
@@ -157,11 +197,15 @@ export class ProfileComponent implements OnInit, OnDestroy {
       friend: this.pseudoFriend,
       login: this.login,
       content: `${this.pseudo} want to be your friend!`,
-      type: "REQUEST_FRIEND"
-    }
-    if (this.profileData.friends.find((friend: Friend) => friend.name === body.login)) {
+      type: 'REQUEST_FRIEND',
+    };
+    if (
+      this.profileData.friends.find(
+        (friend: Friend) => friend.name === body.login
+      )
+    ) {
       console.log(body.friend, 'is already your friend!');
-      return ;
+      return;
     }
     this.socket.emit('send-notif', body);
   }
