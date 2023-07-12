e04e8ba97e959458f747dd9568648e35b1da3533
Louis Truchelut <louis.truchelut@gmail.com>
2023-07-12 18:17:35 +0200

    test

--- api/src/game/game.controller.ts
+++ api/src/game/game.controller.ts
@@ -3,13 +3,12 @@ import { GameService } from './game.service';
 
 @Controller('game')
 export class GameController {
-    constructor(private gameService: GameService) {
-    }
+  constructor(private gameService: GameService) {}
 
-    @Post('private-game')
-    receivePrivateGameData(@Body() gameData: any) {
-        const newGame = this.gameService.createNewGame()
-        newGame.players[0].login = gameData.player1
-        newGame.players[1].login = gameData.player2
-    }
+  @Post('private-game')
+  receivePrivateGameData(@Body() gameData: any) {
+    const newGame = this.gameService.createNewGame();
+    newGame.players[0].login = gameData.player1;
+    newGame.players[1].login = gameData.player2;
+  }
 }
