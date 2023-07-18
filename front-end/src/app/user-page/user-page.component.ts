89cfec99cf3aee7f506787e49c135475c56e13c3
Armandase <damiens.armand@gmail.com>
2023-07-18 19:19:05 +0200

    user page pseudo fixed

--- front-end/src/app/user-page/user-page.component.ts
+++ front-end/src/app/user-page/user-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProfileService } from '../profile/profile.service';
 import { UserData } from '../chat-room/interfaces/interfaces';
+import { UserPageService } from './user-page.service';
 
 @Component({
   selector: 'app-user-page',
@@ -17,7 +18,7 @@ export class UserPageComponent{
   winRate!: string;
   history!: any[];
 
-  constructor (private route: ActivatedRoute, private profileService: ProfileService){
+  constructor (private route: ActivatedRoute, private profileService: ProfileService, private userPageService:UserPageService){
     this.pseudo = "";
     this.route.params.subscribe(params => {
       this.pseudo = params['pseudo'];
@@ -32,10 +33,22 @@ export class UserPageComponent{
       this.profileService.getProfilePic(profileData.login).subscribe((ppData: Blob) => {
         this.ppUrl = URL.createObjectURL(ppData);
       });
+      this.pseudo = profileData.pseudo
       this.status = profileData.status;
       this.stats = profileData.stats;
       this.winRate =  (this.stats.win * 100 / this.stats.matchs).toFixed(2);
       this.history = profileData.history;
+      for (let i in this.history){
+        this.userPageService
+          .getProfileData(this.history[i].opponent)
+          .subscribe((res: any) => {
+            this.history[i].opponent = res.pseudo;
+            if(this.history[i].ownScore > this.history[i].opponentScore)
+              this.history[i].winner = this.pseudo
+            else
+              this.history[i].winner = this.history[i].opponent
+        });
+      }
     })
   }
 }
